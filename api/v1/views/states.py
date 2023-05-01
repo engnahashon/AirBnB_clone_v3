76bec2a8973efca48c0efc02066506aa63f5a581
engnahashon <nahashon.ndirangu@outlook.com>
2023-05-01 22:26:05 +0000

    v3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,3 +13,20 @@ def get_states():
     for state in states.values():
         json_states.append(state.to_dict())
     return jsonify(json_states)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object: GET /api/v1/states/<state_id>"""
+    state = storage.get('State', state_id)
+    if state:
+        return jsonify(state.to_dict())
+    abort(404)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({})
